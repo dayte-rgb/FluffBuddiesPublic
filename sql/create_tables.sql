28a21d8324223c41dcf3713f24b414abf75d8282
David Broczkowski <david.broczkowski@gmail.com>
2026-03-25 13:06:52 -0400

    fixed foreign keys and added not null and unique constraints to half of the create tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,100 +3,103 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS IF NOT EXISTS SecurityQuestion (
   question_id INTEGER PRIMARY KEY,
-  question_text TEXT
+  question_text TEXT NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS IF NOT EXISTS User (
   user_id INTEGER PRIMARY KEY,
-  username TEXT,
-  password TEXT,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL UNIQUE,
   phone_number TEXT,
+  email TEXT UNIQUE,
   zipcode INTEGER,
   profile_description TEXT,
-  account_type CHECK( account_type IN ('pet', 'owner', 'organization', 'user')),
+  account_type CHECK( account_type IN ('pet', 'owner', 'organization', 'user')) NOT NULL,
   profile_picture_link TEXT
 );
 
 CREATE TABLE IF NOT EXISTS IF NOT EXISTS UserMessage (
   message_id INTEGER PRIMARY KEY,
-  sender_id INTEGER,
-  recipient_id INTEGER,
+  sender_id INTEGER NOT NULL,
+  recipient_id INTEGER NOT NULL,
     FOREIGN KEY (recipient_id)
-      REFERENCES User(profile_picture_link),
+      REFERENCES User(user_id),
     FOREIGN KEY (sender_id)
-      REFERENCES User(profile_picture_link)
+      REFERENCES User(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS JobCategory (
   job_category_id INTEGER PRIMARY KEY ,
-  category_name TEXT
+  category_name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS BadgeContent (
   badge_id INTEGER PRIMARY KEY ,
-  badge_name TEXT,
-  badge_image_link TEXT
+  badge_name TEXT NOT NULL UNIQUE,
+  badge_image_link TEXT UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS SkillCategory (
   skill_category_id INTEGER PRIMARY KEY ,
-  category_name TEXT
+  category_name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS SkillCategoryByUser (
-  skill_category_id INTEGER PRIMARY KEY ,
-  user_id INTEGER PRIMARY KEY ,
+  skill_category_id INTEGER PRIMARY KEY NOT NULL,
+  user_id INTEGER PRIMARY KEY NOT NULL,
   
     FOREIGN KEY (skill_category_id)
       REFERENCES SkillCategory(skill_category_id),
   
     FOREIGN KEY (user_id)
-      REFERENCES User(profile_picture_link)
+      REFERENCES User(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS PaymentType (
-  payment_id INTEGER PRIMARY KEY ,
-  payment_name TEXT
+  payment_id INTEGER PRIMARY KEY,
+  payment_name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS JobPayment (
-  job_id INTEGER PRIMARY KEY ,
-  payment_id INTEGER,
-  payment_quantity INTEGER,
-  
+  job_id INTEGER PRIMARY KEY NOT NULL,
+  payment_id INTEGER NOT NULL,
+  payment_quantity INTEGER NOT NULL,
+    FOREIGN KEY (job_id)
+      REFERENCES JobContent(job_id),
     FOREIGN KEY (payment_id)
       REFERENCES PaymentType(payment_id)
 );
 
 CREATE TABLE IF NOT EXISTS JobCategoryByUser (
-  job_category_id INTEGER PRIMARY KEY ,
-  user_id INTEGER PRIMARY KEY ,
+  job_category_id INTEGER PRIMARY KEY NOT NULL,
+  user_id INTEGER PRIMARY KEY NOT NULL,
   
     FOREIGN KEY (job_category_id)
       REFERENCES JobCategory(job_category_id),
   
     FOREIGN KEY (user_id)
-      REFERENCES User(profile_picture_link)
+      REFERENCES User(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS LeaderboardContent (
   leaderboard_id INTEGER PRIMARY KEY ,
-  reward_badge_id INTEGER,
-  start_time TEXT,
-  end_time TEXT
+  reward_badge_id INTEGER NOT NULL,
+  start_time TEXT NOT NULL,
+  end_time TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS UserBadge (
-  user_id INTEGER PRIMARY KEY ,
-  badge_id INTEGER PRIMARY KEY ,
-  
+  user_id INTEGER PRIMARY KEY NOT NULL,
+  badge_id INTEGER PRIMARY KEY NOT NULL,
+    FOREIGN KEY (user_id)
+      REFERENCES User(user_id),
     FOREIGN KEY (badge_id)
       REFERENCES BadgeContent(badge_id)
 );
 
 CREATE TABLE IF NOT EXISTS CertificationContent (
   certification_id INTEGER PRIMARY KEY ,
-  certification_name TEXT,
+  certification_name TEXT NOT NULL UNIQUE,
   company TEXT
 );
 
@@ -106,24 +109,29 @@ CREATE TABLE IF NOT EXISTS JobContent (
   datetime TEXT,
   duration INTEGER,
   zipcode INTEGER,
-  employee_num INTEGER,
-  job_filled BOOLEAN
+  employee_num INTEGER NOT NULL,
+  job_filled BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS SkillCategoriesByJob (
-  job_id INTEGER PRIMARY KEY ,
-  skill_category_id INTEGER PRIMARY KEY ,
+  job_id INTEGER PRIMARY KEY NOT NULL,
+  skill_category_id INTEGER PRIMARY KEY NOT NULL,
   
     FOREIGN KEY (skill_category_id)
       REFERENCES SkillCategory(skill_category_id),
   
     FOREIGN KEY (job_id)
-      REFERENCES JobContent(job_filled)
+      REFERENCES JobContent(job_id)
 );
 
 CREATE TABLE IF NOT EXISTS JobReview (
-  review_id INTEGER PRIMARY KEY ,
-  job_id INTEGER
+  review_id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+  job_id INTEGER NOT NULL UNIQUE
+
+    FOREIGN KEY(review_id)
+      REFERENCES ReviewContent(review_id),
+    FOREIGN KEY (job_id)
+      REFERENCES JobContent(job_id)
 );
 
 CREATE TABLE IF NOT EXISTS ReviewContent (
@@ -132,39 +140,43 @@ CREATE TABLE IF NOT EXISTS ReviewContent (
   quality INTEGER,
   friendliness INTEGER,
   comments TEXT,
-  datetime TEXT,
-  verified BOOLEAN
+  datetime TEXT NOT NULL,
+  verified BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS OrganizationMember (
-  org_id INTEGER PRIMARY KEY ,
-  user_id INTEGER PRIMARY KEY ,
+  org_id INTEGER PRIMARY KEY NOT NULL,
+  user_id INTEGER PRIMARY KEY NOT NULL,
   
     FOREIGN KEY (org_id)
-      REFERENCES User(profile_picture_link),
+      REFERENCES User(user_id),
   
     FOREIGN KEY (user_id)
-      REFERENCES User(profile_picture_link)
+      REFERENCES User(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS EmployerJob (
-  job_id INTEGER PRIMARY KEY,
-  employer_id INTEGER,
-  
+  job_id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+  employer_id INTEGER NOT NULL UNIQUE,
+
+    FOREIGN KEY (job_id)
+      REFERENCES JobContent(job_id),
     FOREIGN KEY (employer_id)
       REFERENCES User(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS JobCategoriesByJob (
-  job_id INTEGER PRIMARY KEY ,
-  job_category_id INTEGER PRIMARY KEY ,
+  job_id INTEGER PRIMARY KEY NOT NULL,
+  job_category_id INTEGER PRIMARY KEY NOT NULL,
   
     FOREIGN KEY (job_id)
-      REFERENCES JobContent(job_filled),
+      REFERENCES JobContent(job_id),
   
     FOREIGN KEY (job_category_id)
       REFERENCES JobCategory(job_category_id)
 );
+
+-- CONTINUE BELOW
 
 CREATE TABLE IF NOT EXISTS MessageContent (
   message_id INTEGER PRIMARY KEY,
