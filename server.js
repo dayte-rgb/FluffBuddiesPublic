1cdf0c47e064abf8d7fa041a8cf352b636b53b48
David Broczkowski <david.broczkowski@gmail.com>
2026-04-04 21:43:26 -0400

    added logging to server.js for connections and status, still in progress

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 // Import the Express module, which is a framework for building web applications in Node.js.
 const express = require('express');
 const path = require('path');
+const Log = require('./tools/log.js');
+const { start } = require('repl');
+const logger = new Log('requests.log', true);
 
 // Create an instance of an Express application. This app object will be used to define routes and middleware.
 const app = express();
 
 // Define a constant for the port number on which the server will listen.
 const PORT = 3000;
+
+// Middleware for parsing HTTP responses
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware for logging
+app.use(function (req, res, next) {
+  const startTime = Date.now();
+  logger.write(`[INFO] Route: ${req.url} Method: ${req.method}`);
+  next();
+});
 
 // Middleware for handling static files
 app.use(express.static('public'));
@@ -15,14 +29,10 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Start the server and make it listen on the specified port.
-// Once the server starts, it logs a message to the console indicating where it is running.
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.status(200);
+  write_res_log(res);
+  res.redirect('index.html');
 });
 
 // Define a route handler for GET requests to the URL ('/default')
@@ -33,3 +43,14 @@ app.get('/default', (req, res) => {
         items: ['Apples', 'Bananas', 'Cherries'] // An array named 'items' containing a list of fruits.
     });
 });
+
+function write_res_log(res){
+  logger.write(`[INFO] Returned Status Code: ${res.statusCode}`);
+  return;
+}
+// Start the server and make it listen on the specified port.
+// Once the server starts, it logs a message to the console indicating where it is running.
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
