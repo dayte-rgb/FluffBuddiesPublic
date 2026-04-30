04259280f70cc9042cb7c7250f5406c46fe4aef5
David Broczkowski <david.broczkowski@gmail.com>
2026-04-29 20:46:01 -0400

    add model to get users that have an open conversation and history of a conversation for messaging system

---
+++ models/messagingModel.js
@@ -0,0 +1,39 @@
+const {connectToDatabase } = require('../database');
+
+class MessagingModel {
+    constructor() {
+        this.db = connectToDatabase();
+
+        this.historyQuery = this.db.prepare(`
+            SELECT msg.message_id, msg.message_content, msg.datetime, um.sender_id, um.recipient_id
+            FROM MessageContent msg
+            JOIN UserMessage um ON msg.message_id = um.message_id
+            WHERE (um.sender_id = ? AND um.recipient_id = ?) OR (um.sender_id = ? AND um.recipient_id = ?)
+            ORDER BY msg.datetime ASC;
+        `);
+        
+        // find where the user has been both the sender and recipient and union
+        // this represents all conversations the user has been in
+        this.conversationQuery = this.db.prepare(`
+            SELECT DISTINCT um.recipient_id AS contact_id, u.username AS contact_username
+            FROM UserMessage um
+            JOIN User u ON um.recipient_id = u.user_id
+            WHERE um.sender_id = ?
+            UNION
+            SELECT DISTINCT sender_id AS contact_id, u.username AS contact_username
+            FROM UserMessage um
+            JOIN User u ON um.sender_id = u.user_id
+            WHERE um.recipient_id = ?;
+        `);
+    }
+
+    getHistory(user1_id, user2_id){
+        return this.historyQuery.all(user1_id, user2_id, user2_id, user1_id);
+    }
+    
+    getConversationIds(user_id){
+        return this.conversationQuery.all(user_id, user_id);
+    }
+}
+
+module.exports = MessagingModel;
