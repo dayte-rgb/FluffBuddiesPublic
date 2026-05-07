3532abb5030d9085c735fb00b20b38b344fb9e54
David Broczkowski <david.broczkowski@gmail.com>
2026-05-07 00:43:50 -0400

    add review model that returns the username of the employee and review info

---
+++ models/reviewModel.js
@@ -0,0 +1,22 @@
+const { connectToDatabase } = require('../database');
+
+class reviewModel {
+  constructor() {
+    this.db = connectToDatabase();
+
+    this._getReviews = this.db.prepare(`
+        SELECT u.username, jr.review_id, rc.punctuality, rc.quality, rc.friendliness, rc.comments, rc.datetime, rc.verified
+        FROM JobReview jr
+        JOIN ReviewContent rc ON jr.review_id = rc.review_id
+        JOIN UserReview ur ON jr.review_id = ur.review_id
+        JOIN User u ON ur.user_id = u.user_id
+        WHERE jr.job_id = ?;
+    `);
+  }
+
+  getReviewsByJobId(job_id){
+    return this._getReviews.all(job_id);
+  }
+}
+
+module.exports = reviewModel;
