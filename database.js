48b45a7503fa9cbfc504930acce5aa81e117f0a8
David Broczkowski <david.broczkowski@gmail.com>
2026-05-12 20:55:38 -0400

    useless commits?

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 
 // Function to create and return a database connection
 function connectToDatabase() {
-  const db = new Database('./demo.db');
+  const db = new Database('./website.db');
   
   // Enable foreign keys
   db.exec('PRAGMA foreign_keys = ON');
@@ -12,6 +12,10 @@ function connectToDatabase() {
 
 // Function to close the database connection
 function closeDatabase(db) {
+  if(db == undefined || !(db instanceof Database)){
+    console.log("[ERROR] Attempted to close the database, but the database was undefined");
+    return;
+  }
   db.close();
 }
 
